d18470c8f6383c58cb411bfcd154079d640d2a02
Sadik Khan <sadikdevelopersevltose15@gmail.com>
2024-01-30 18:50:27 +0530

    content

--- js/testimonial.js
+++ js/testimonial.js
@@ -15,36 +15,36 @@ let people = [
 		photo:
 			'url("https://cdn.pixabay.com/photo/2018/03/06/22/57/portrait-3204843_960_720.jpg")',
 		name: "Susan Smith",
-		profession: "WEB DEVELOPER",
+		profession: "Ayurveda is Best",
 		description:
-			"Cheese and biscuits chalk and cheese fromage frais. Cheeseburger caerphilly cheese slices chalk and cheese cheeseburger mascarpone danish fontina rubber cheese. Squirty cheese say cheese manchego jarlsberg lancashire taleggio cheese and wine squirty cheese. Babybel pecorino feta macaroni cheese brie queso everyone loves gouda. Cheese and biscuits camembert de normandie fromage fromage macaroni cheese"
+			"I came to futureHealth Hub for my Arthritis treatment after spending a lot of money ondoctors and medicines .And now I can proudly say that Ayurveda really works."
 	},
 
-	{
-		photo:
-			"url('https://cdn.pixabay.com/photo/2019/02/11/20/20/woman-3990680_960_720.jpg')",
-		name: "Anna Grey",
-		profession: "UFC FIGHTER",
-		description:
-			"I'm baby migas cornhole hell of etsy tofu, pickled af cardigan pabst. Man braid deep v pour-over, blue bottle art party thundercats vape. Yr waistcoat whatever yuccie, farm-to-table next level PBR&B. Banh mi pinterest palo santo, aesthetic chambray leggings activated charcoal cred hammock kitsch humblebrag typewriter neutra knausgaard. Pabst succulents lo-fi microdosing portland gastropub Banh mi pinterest palo santo"
-	},
+	// {
+	// 	photo:
+	// 		"url('https://cdn.pixabay.com/photo/2019/02/11/20/20/woman-3990680_960_720.jpg')",
+	// 	name: "Anna Grey",
+	// 	profession: "UFC FIGHTER",
+	// 	description:
+	// 		"I'm baby migas cornhole hell of etsy tofu, pickled af cardigan pabst. Man braid deep v pour-over, blue bottle art party thundercats vape. Yr waistcoat whatever yuccie, farm-to-table next level PBR&B. Banh mi pinterest palo santo, aesthetic chambray leggings activated charcoal cred hammock kitsch humblebrag typewriter neutra knausgaard. Pabst succulents lo-fi microdosing portland gastropub Banh mi pinterest palo santo"
+	// },
 
 	{
 		photo:
 			"url('https://cdn.pixabay.com/photo/2016/11/21/12/42/beard-1845166_960_720.jpg')",
 		name: "Branson Cook",
-		profession: "ACTOR",
+		profession: "BP & Sugar Over",
 		description:
-			"Radio telescope something incredible is waiting to be known billions upon billions Jean-François Champollion hearts of the stars tingling of the spine. Encyclopaedia galactica not a sunrise but a galaxyrise concept of the number one encyclopaedia galactica from which we spring bits of moving fluff. Vastness is bearable only through love paroxysm of global death concept"
+			"Mujhe kaffi time se BP aur sugar ki problem thi. Main futureHealth Hub me aayan aur yahan se apna treatment shuru krwaya.Sirf 5 mahine me sugar"
 	},
 
 	{
 		photo:
 			"url('https://cdn.pixabay.com/photo/2014/10/30/17/32/boy-509488_960_720.jpg')",
 		name: "Julius Grohn",
-		profession: "PROFESSIONAL CHILD",
+		profession: "Got Quick Releif From Constipation",
 		description:
-			"Biscuit chocolate pastry topping lollipop pie. Sugar plum brownie halvah dessert tiramisu tiramisu gummi bears icing cookie. Gummies gummi bears pie apple pie sugar plum jujubes. Oat cake croissant bear claw tootsie roll caramels. Powder ice cream caramels candy tiramisu shortbread macaroon chocolate bar. Sugar plum jelly-o chocolate dragée tart chocolate marzipan cupcake gingerbread."
+			"I was suffering from constipation.I visited futureHealth Hub and started my treatment. Within 1 month I got relief from my constipation problem."
 	}
 ];
 
